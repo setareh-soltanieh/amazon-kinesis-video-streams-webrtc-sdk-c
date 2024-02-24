f2115965c802318e386fa08333b539de2b71b5b9
setareh <setarehsoltanieh78@gmail.com>
2024-02-24 15:10:26 -0500

    Initial commit

---
+++ build/my_webrtc/CMakeFiles/kvsWebrtcClientViewer.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/setareh/catkin_ws/src/my_webrtc/src/Common.cpp" "/home/setareh/catkin_ws/build/my_webrtc/CMakeFiles/kvsWebrtcClientViewer.dir/src/Common.cpp.o"
+  "/home/setareh/catkin_ws/src/my_webrtc/src/kvsWebRTCClientViewer.cpp" "/home/setareh/catkin_ws/build/my_webrtc/CMakeFiles/kvsWebrtcClientViewer.dir/src/kvsWebRTCClientViewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CMAKE_DETECTED_CACERT_PATH"
+  "KVS_CA_CERT_PATH=\"/home/setareh/catkin_ws/src/my_webrtc/certs/cert.pem\""
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"my_webrtc\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/setareh/catkin_ws/src/my_webrtc/open-source/include"
+  "/home/setareh/catkin_ws/src/my_webrtc/include/my_webrtc"
+  "/opt/ros/noetic/include"
+  "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/home/setareh/New/amazon-kinesis-video-streams-webrtc-sdk-c/build"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/my_webrtc/CMakeFiles/kvsWebrtcClientViewer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/setareh/catkin_ws/devel/lib/my_webrtc/kvsWebrtcClientViewer"
+  "/home/setareh/catkin_ws/devel/lib/my_webrtc/kvsWebrtcClientViewer.pdb"
+  "CMakeFiles/kvsWebrtcClientViewer.dir/src/Common.cpp.o"
+  "CMakeFiles/kvsWebrtcClientViewer.dir/src/kvsWebRTCClientViewer.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kvsWebrtcClientViewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
